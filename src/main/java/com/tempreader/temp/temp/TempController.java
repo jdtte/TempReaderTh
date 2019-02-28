4ccc709616e34639741e190266e74f51ef61fe32
JDAIN <poker98@web.de>
2019-02-28 14:31:11 +0100

    added swagger 2 docs

--- src/main/java/com/tempreader/temp/temp/TempController.java
+++ src/main/java/com/tempreader/temp/temp/TempController.java
@@ -1,5 +1,6 @@
 package com.tempreader.temp.temp;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -9,31 +10,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@Api(value = "temps", description = "Operations pertaining Temps")
 public class TempController {
     @Autowired
     TempService tempservice;
 
-    @GetMapping("/api/temps")
+    @GetMapping("/temps")
     @ApiOperation(value = "Show all Temps", response = Temp.class)
     @ApiResponses(value = {
-     @ApiResponse(code = 200, message = "Server reachable")})
-    public List<Temp> getTemps(){
+            @ApiResponse(code = 200, message = "Server reachable")})
+    public List<Temp> getTemps() {
         return tempservice.getTemps();
     }
 
-    @GetMapping("api/temps/{id}")
-    @ApiOperation(value="Show 1 Temp", response = Temp.class)
-    public Temp getTemp(@PathVariable int id ){
+    @GetMapping("/temps/{id}")
+    @ApiOperation(value = "Search and Show 1 Temp with the Id", response = List.class)
+    public Temp getTemp(@PathVariable int id) {
         return tempservice.getTemp(id);
     }
 
-    @PutMapping(value = "/api/temps/{id}", consumes = "application/json", produces = "application/json")
-    public void updateTemp(@RequestBody Temp temp){
+    @PutMapping(value = "/temps/{id}", consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Updates for a Temp with the Id", response = Temp.class)
+    public void updateTemp(@RequestBody Temp temp) {
         tempservice.updateTemp(temp);
     }
 
-    @PostMapping(value = "api/temps/add",consumes = "application/json", produces = "application/json")
-    public void addTemp(@RequestBody Temp temp){
+    @PostMapping(value = "/temps/add", consumes = "application/json", produces = "application/json")
+    @ApiOperation(value= "Add a Temp")
+    public void addTemp(@RequestBody Temp temp) {
         tempservice.addTemp(temp);
     }
 
