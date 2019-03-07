771f96754448f0ba69732ccaa2bfa57650fce51f
JDAIN <poker98@web.de>
2019-03-07 16:53:08 +0100

    template avg all added

--- Backend/src/main/java/com/tempreader/temp/temp/TempController.java
+++ Backend/src/main/java/com/tempreader/temp/temp/TempController.java
@@ -28,10 +28,10 @@ public class TempController {
         model.addAttribute("avgHumidityIn24Hour", tempService.getAverageHumidityInDurationHours(24));
         //avg168hrs = 7 Tage
         model.addAttribute("avgTempsIn168Hour", tempService.getAverageTempInDurationHours(168));
-        model.addAttribute("avgHumidityIn168Hour", tempService.getAverageTempInDurationHours(168));
+        model.addAttribute("avgHumidityIn168Hour", tempService.getAverageHumidityInDurationHours(168));
         //avg720hrs = 30 Tage
         model.addAttribute("avgTempsIn720Hour", tempService.getAverageTempInDurationHours(720));
-        model.addAttribute("avgHumidityIn720Hour", tempService.getAverageTempInDurationHours(720));
+        model.addAttribute("avgHumidityIn720Hour", tempService.getAverageHumidityInDurationHours(720));
         model.addAttribute("temps",tempService.getTemps());
         model.addAttribute("t",t);
         return "tempStart";
