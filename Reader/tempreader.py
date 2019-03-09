cac2ec65a8c1e0a18e4277d9dae6af0ff956e8bd
JDAIN <poker98@web.de>
2019-03-10 00:04:17 +0100

    improved reader

--- Reader/tempreader.py
+++ Reader/tempreader.py
@@ -3,22 +3,18 @@ import time
 import datetime
 import io
 import json
+import requests
+
+API_ENDPOINT = ""
 
 
 def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
 
 
-def printer():
-    print('\r{:5d}  Humidity: {:2.1f}%  temp: {:2.1f}°C  {}'.format(humidity, temp,
-                                                                    datetime.datetime.now().strftime(
-                                                                        '%d.%m.%y %H:%M:%S')), end='')
-
-
 def api_poser():
+    return null
 
-
-    # TODO
 
 def avg_list_min():
     min_counter = 0;
@@ -33,17 +29,15 @@ if __name__ == '__main__':
 
     while True:
         if time.time() - start > 60:
-            print("60s over")
-            start = time.time()
+            data = {}
+            postreq = requests.post(url=API_ENDPOINT, data=data)
+            start = time.time()  # restarts timer
 
-            print(mean(minHumiditylist), mean(minTemplist), "  60 sec avg")
+            print(mean(minHumiditylist), mean(minTemplist), postreq.status_code)
             minTemplist = []
             minHumiditylist = []
 
-        humidity, temp = Adafruit_DHT.read(Adafruit_DHT.DHT22, 2)
-        if (temp and humidity):
+        humidity, temp = Adafruit_DHT.read(Adafruit_DHT.DHT22, 2)  # reads data
+        if temp and humidity:  # checks if error/empty vals
             minHumiditylist.append(humidity)
             minTemplist.append(temp)
-
-        # printer()
-        # print('\r{:5d}  {:2.1f}%  {:2.1f}°C  {}'.format(temp_id, humidity, temp, datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S')), end='')
